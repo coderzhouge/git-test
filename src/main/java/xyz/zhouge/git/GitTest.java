30ee4c62707f57b65fc91cd092f68058399ed36c
zhouge1 <zhouge1@git.com>
2022-04-28 12:36:00 +0800

    hot-fix commit1

--- src/main/java/xyz/zhouge/git/GitTest.java
+++ src/main/java/xyz/zhouge/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello ,Git !");
         System.out.println("This is my first Git in IDEA .");
+        System.out.println("hot-fix commit1");
     }
 }
