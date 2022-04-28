1a06ddf3432c6c4b8bdf2b28f3c1498ac68ed611
zhouge1 <zhouge1@git.com>
2022-04-28 11:56:38 +0800

    second commit by zhouge

---
+++ src/main/java/xyz/zhouge/git/GitTest.java
@@ -0,0 +1,15 @@
+package xyz.zhouge.git;
+
+/**
+ * @BelongsProject: git-test
+ * @BelongsPackage: xyz.zhouge.git
+ * @CreateTime: 2022-04-28  11:52
+ * @Description: TODO
+ * @Version: 1.0
+ * @Author：zhouge
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello ,Git !");
+    }
+}
